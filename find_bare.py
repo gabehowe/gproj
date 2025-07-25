494e8389eb37e90cfcbf6f44144b67933d40c5d7
Gabriel Howe <me@gabe.how>
2025-07-25 17:05:00 -0400

    change finder to maybe work better somehow

--- find_bare.py
+++ find_bare.py
@@ -2,18 +2,21 @@ import argparse
 import shutil
 import sys
 
+import colorama
+
 import database
 import os
+
 
 def filter_func(it, term):
     val = term in (" ".join(it.stringinate().values())).lower()
     return val
 
 
-
 def filter_for_term(term, options):
-    options = list(filter(lambda it: filter_func(it, term), options))
-    return options
+    filtered_options = list(filter(lambda it: filter_func(it, term), options))
+    return filtered_options
+
 
 if __name__ == '__main__':
     database.database_file = '/home/gabri/dev/database/index.gproj'
@@ -25,7 +28,7 @@ if __name__ == '__main__':
             options = filter_for_term(i.removesuffix('"').removeprefix('"'), options)
 
     if len(options) > 1:
-        print("Too ambiguous!",file=sys.stderr)
+        print("Too ambiguous!", file=sys.stderr)
         sys.exit(-1)
     elif len(options) == 0:
         print("Couldn't find", file=sys.stderr)
--- finder.py
+++ finder.py
@@ -1,44 +1,58 @@
-import argparse
-import shutil
+import os
 import sys
-
+from typing import Callable
 
 import colorama
 from colorama import Style
-from colorama import Fore
 
 import database
-import os
+import find_bare
 
-def filter_for_term(term, options):
-    last_options = options.copy()
-    options = list(filter(lambda it: term in (" ".join(it.stringinate().values())).lower(), options))
-    unfound = term if len(options) == 0 else None
-    if unfound:
-        options = last_options
-    return options, unfound
+# TODO: replace with generator
+def try_term(term: Callable[[], str], options, interactive: bool = False):
+    """
+    try to filter options based on term.
+    """
+    try:
+        term_val = term()
+    except StopIteration:
+        return options
+
+    filtered_options = find_bare.filter_for_term(term_val, options)
+    if len(filtered_options) == 0:
+        if interactive:
+            database.print_table(options)
+        print(colorama.Fore.RED + f"No results for {term_val}." + colorama.Fore.RESET)
+        return try_term(term, options, interactive)
+
+    if len(filtered_options) > 1:
+        if interactive:
+            database.print_table(filtered_options)
+        return try_term(term, filtered_options, interactive)
+
+    return filtered_options
+
+# try to filter each pre-option
+# if pre-option doesn't filter, restore old state and try to filter with next pre-option
+# then, ask user
 
 if __name__ == '__main__':
     print(Style.BRIGHT + "Welcome to Project Finder!" + Style.RESET_ALL)
-    database.database_file = '/home/gabri/dev/database/index.gproj'
+    # TODO: throw error if PROJECT_DATABASE is not set
+    database.database_file = os.environ["PROJECT_DATABASE"] + '/index.gproj'
     options = database.parse_file()
     unfound = False
-    if len(sys.argv) > 1:
-        for i in sys.argv[1:]:
-            options = filter_for_term(i, options)
+    # prefilter based on command line arguments
+    if len(sys.argv) > 2:
+        itr = iter(sys.argv[2:])
+        options = try_term(itr.__next__, options, False)
+    database.print_table(options)
 
-    while len(options) > 1:
-        database.print_table(options)
-        if unfound:
-            print(colorama.Fore.RED + f"No results for {unfound}." + colorama.Fore.RESET)
-            unfound = None
-        term = input("term: ").lower()
-        options, unfound = filter_for_term(term, options)
+    if len(options) > 1:
+        options = try_term(lambda: input("term: ").lower(), options, True)
 
-    # os.system(f'cd /home/gabri/dev/database/temp/{options[0].id}')
     print(f'{ options[0].title }/{options[0].creation}')
-    tf_loc = "/tmp/searchtempfile"
-    if os.path.exists(tf_loc):
-        os.remove(tf_loc)
-    with open(tf_loc, 'x+') as tempfile:
-        tempfile.write(f'{os.environ["PROJECT_DATABASE"]}/{options[0].id}')
+    if len(sys.argv) > 1:
+        tf_loc = sys.argv[1]
+        with open(tf_loc, 'w+') as tempfile:
+            tempfile.write(f'{os.environ["PROJECT_DATABASE"]}/{options[0].id}')
